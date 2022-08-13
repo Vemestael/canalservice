d300cb4fc17eff22143f3c198c076a8ac8d64f3f
Vemestael <volodimir.grigoryev@gmail.com>
2022-08-13 14:50:46 +0300

    delete unused code

--- src/main/apps.py
+++ src/main/apps.py
@@ -4,11 +4,3 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
-
-    def ready(self):
-        """
-        Вызывает обновление данных из таблицы при каждом запуске сервера
-        """
-#        from main.views import GSheetsAPI
-#        GSheetsAPI().get('')
-        pass
